58992d4bf4c93dd62441601174480507674d6dfd
Mordy Pesach <mordypassover@gmail.com>
2026-07-15 11:06:13 +0300

    started worcking on pipline

---
+++ src/PipLine/PipLine.cs
@@ -0,0 +1,10 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace DroneFleetDataProcessing.pipline;
+
+class Piline
+{
+    private string _data;
+    private string _outputPath;
+   // private Validator _validator;
+}
