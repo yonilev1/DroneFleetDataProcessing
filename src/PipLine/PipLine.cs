f9489028522bbd8728b130cb9af365bb2a0e209c
Mordy Pesach <mordypassover@gmail.com>
2026-07-15 12:18:06 +0300

    fuxed namespace

--- src/PipLine/PipLine.cs
+++ src/PipLine/PipLine.cs
@@ -1,5 +1,5 @@
 using System.ComponentModel.DataAnnotations;
-using DroneFleetDataProcessingSystem.ValidatorClass;
+using DroneFleetDataProcessing.ValidatorClass;
 using DroneFleetDataProcessing.drone;
 
 namespace DroneFleetDataProcessing.pipline;
