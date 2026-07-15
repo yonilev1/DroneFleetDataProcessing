6c29bdf8b0fbbe1e759ef794d3e98f962df10d8f
Mordy Pesach <mordypassover@gmail.com>
2026-07-15 12:56:47 +0300

    added methods to pipeline

--- src/PipLine/PipLine.cs
+++ src/PipLine/PipLine.cs
@@ -1,23 +1,74 @@
-using System.ComponentModel.DataAnnotations;
-using DroneFleetDataProcessing.ValidatorClass;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.Json;
 using DroneFleetDataProcessing.drone;
+using DroneFleetDataProcessing.ValidatorClass;
 
-namespace DroneFleetDataProcessing.pipline;
-
-class Pipline
+namespace DroneFleetDataProcessing.pipeline
 {
-    public string Data { get; set; }
-    public string OutputPath { get; set; }
-    public DroneValidator Validator { get; set; }
-    public List<Drone> ValidDroneReports { get; set; }
-    public int RegectedCount { get; set; }
-
-    public Pipline(string data, string outputPath)
+    class Pipeline
     {
-        Data = data;
-        OutputPath = outputPath;
-        ValidDroneReports = new List<Drone>();
-        Validator = new DroneValidator(ValidDroneReports);
+        public string Data { get; set; }
+        public string OutputPath { get; set; }
+        public DroneValidator Validator { get; set; }
+        public List<Drone> ValidDroneReports { get; set; }
+        public int RejectedCount { get; set; }
 
+        public Pipeline(string data, string outputPath)
+        {
+
+            Data = data;
+            OutputPath = outputPath;
+            ValidDroneReports = new List<Drone>();
+            Validator = new DroneValidator(ValidDroneReports);
+        }
+
+        private void Parse()
+        {
+            try
+            {
+
+                List<Drone> parsedReports = JsonSerializer.Deserialize<List<Drone>>(Data);
+
+                foreach (Drone drone in parsedReports)
+                {
+                    if (drone == null) continue;
+
+                    
+                    if (!Validator.Excecute(drone))
+                    {
+                        RejectedCount++;
+                    }
+                }
+            }
+            catch (JsonException ex)
+            {
+
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        private void WriteNewFile()
+        {
+            try
+            {
+                
+                var options = new JsonSerializerOptions { WriteIndented = true };
+                string json = JsonSerializer.Serialize(ValidDroneReports, options);
+
+                File.WriteAllText(OutputPath, json);
+            }
+            catch (IOException ex)
+            {
+                Console.WriteLine($"Error writing to file {OutputPath}: {ex.Message}");
+            }
+        }
+
+        public void Execute()
+        {
+            Parse();
+            WriteNewFile();
+        }
     }
 }
