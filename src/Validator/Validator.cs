3ec6417b46e1a761858b8324fed71f234af8727a
Meir <meirroc5764@gmail.com>
2026-07-15 10:51:50 +0300

    first functon validate isValidId

---
+++ src/Validator/Validator.cs
@@ -0,0 +1,61 @@
+using System;
+using DroneFleetDataProcessing.drone;
+namespace DroneFleetDataProcessingSystem.ValidatorClass
+{
+    class Validator
+    {
+        private List<Drone> _validatedDrones;
+        public Validator(List<Drone> drones)
+        {
+            
+            _validatedDrones = drones;
+        }
+        public bool Excecute(Drone drone)
+        {
+            return true;
+        }
+        private bool IsValidId(int id)
+        {
+            if (_validatedDrones.Any(x => x.id == id) || id < 1)
+                return false;
+            
+            return true;
+        }
+        private bool IsvalidSerialNumber(string SN)
+        {
+
+        }
+        private bool IsValidModel(string model)
+        {
+
+        }
+        private bool IsValidCategory(string category)
+        {
+
+        }
+        private bool isValidBaseLocatoins(string baseLocations)
+        {
+
+        }
+        private bool isValidFlithHours(double flithHours)
+        {
+
+        }
+        private bool isValidBaterryHelth(int baterryHlth)
+        {
+
+        }
+        private bool isValidMaxRenge(int maxRenge)
+        {
+
+        }
+        private bool isValidMissionsCompleted(int missionsCompleted)
+        {
+
+        }
+        private bool isValidStatus(string status)
+        {
+
+        }
+    }
+}
